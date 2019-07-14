bb86d1acf6cf120a0e18fd107b83d09969ba5205
Allen B <pingguo.ny@gmail.com>
2019-07-13 22:13:52 -0400

    Comments

--- server.go
+++ server.go
@@ -1,20 +1,25 @@
+// countries.io
+// Copyright (C) 2019 Allen B
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 /*
-countries.io
-Copyright (C) 2019 Allen B
+This is the game of countries.io. You can access a demo of this at http://countriesio.xyz/
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
+This program will serve the game on localhost:$PORT. If $PORT is not set, the program will serve on port 8080.
 */
+
 package main
 
 import (
